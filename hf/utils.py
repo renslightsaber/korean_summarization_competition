7754d8a8e2042eef305fc0e462223e001ff4edbe
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-03-30 21:48:18 +0900

    Update utils.py

--- hf/utils.py
+++ hf/utils.py
@@ -14,7 +14,19 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
-
+#################### Set Seed ###########################
+def set_seed(seed=42):
+    '''Sets the seed of the entire notebook so results are the same every time we run.
+    This is for REPRODUCIBILITY.'''
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    
 ################### MyDataset #######################
 class MyDataset(Dataset):
     def __init__(self, 
