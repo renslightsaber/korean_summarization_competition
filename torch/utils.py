bdedd78297e7722408b81577424a5c16a42d002e
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-04-01 12:38:41 +0900

    Update utils.py

--- torch/utils.py
+++ torch/utils.py
@@ -234,6 +234,7 @@ def get_model(model_name,
 ######################## summarize function ###########################
 def summarize(model, 
               accelerator, 
+              tokenizer,
               dataloader, 
               num_beams,
               device):
